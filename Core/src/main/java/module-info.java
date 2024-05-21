d6c59cb81505f0cde5bb97afe9dce527e5a2a518
Marcus Boel Quebec Jürgensen <54073818+mbqj@users.noreply.github.com>
2024-05-21 16:12:57 +0200

    Spring works and microservice generated

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -9,7 +9,9 @@ module Core {
     requires spring.core;
     requires java.net.http;
 
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    exports dk.sdu.mmmi.cbse.main;
+
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
 
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
---
+++ scoringSystem/src/main/java/dk/sdu/mmmi/cbse/scoringSystem/ScoringSystemApplication.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.scoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringSystemApplication.class, args);
+	}
+
+}
